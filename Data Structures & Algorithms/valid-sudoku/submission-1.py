14e919c293b1afc772bd1a23351913ccc2230502
DavidXU12345 <david.xuhengwei@gmail.com>
2026-05-16 18:23:55 -0400

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check duplicates in rows, cols and squares
+        # Time complexity O(n^2)
+        rows = [[False] * 10 for _ in range(9)]
+        cols = [[False] * 10 for _ in range(9)]
+        squares = [[False] * 10 for _ in range(9)]
+
+        for col in range(9):
+            for row in range(9):
+                if board[row][col] == '.':
+                    continue
+                num = int(board[row][col])
+                if rows[row][num] or cols[col][num] or squares[row // 3 * 3 + col // 3][num]:
+                    return False
+                rows[row][num] = True
+                cols[col][num] = True
+                squares[row // 3 * 3 + col // 3][num] = True
+        return True
+        
