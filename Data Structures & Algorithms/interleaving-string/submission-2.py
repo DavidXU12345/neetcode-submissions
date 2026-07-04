756da975e07b1c709930d7d8b5a2c2c43bcc44b1
DavidXU12345 <david.xuhengwei@gmail.com>
2026-07-04 11:25:15 -0400

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s3) != len(s1) + len(s2):
+            return False
+        @lru_cache(None)
+        def dfs(i, j, k):
+            if k == len(s3):
+                return (i == len(s1)) and (j == len(s2))
+
+            res = False
+            if i < len(s1) and s3[k] == s1[i]:
+                res |= dfs(i + 1, j, k + 1)
+            
+            if j < len(s2) and s3[k] == s2[j]:
+                res |= dfs(i, j + 1, k + 1)
+            
+            return res
+        
+        return dfs(0, 0, 0)
